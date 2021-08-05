72075ae73369fe4e2cd27f52191c8dfcbca3873a
ocang <ocang.nutech@gmail.com>
2021-08-05 09:41:57 +0700

    add task router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { response } from "express";
 import config from "config";
 import cors from "cors"
-import { v4 as uuidv4 } from 'uuid';
 import logger from "morgan";
+import { tasksRouter } from "./modules/tasks/tasks.router";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
@@ -13,70 +13,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-interface todoData {
-  id: string,
-  title: string,
-  description: string | null,
-  complete: boolean
-}
-
-const storage: Array<todoData> = [];
-
-const addTodo = (data: todoData, storage: Array<todoData>) => {
-
-  storage.push(data);
-  return true;
-}
-
-const deleteTodo = (id: string, storage: Array<todoData>) => {
-  const index = storage.findIndex(el => id === id);
-
-  if (index === -1) return false;
-
-  storage.splice(index, 1);
-  return true;
-
-}
-
-const updateTodo = (id: string, storage: Array<todoData>) => {
-  const item = storage.find(el => id === id);
-
-  if (!item) return false;
-
-  item.complete = (!item.complete);
-  return true;
-}
-
-app.get('/', (req, res) => {
-  const response = JSON.stringify(storage);
-  res.status(200).send(response);
-})
-
-app.post('/', (req, res) => {
-  const id = uuidv4();
-  const data = { id, ...req.body };
-  const add = addTodo(data, storage);
-  if (!add) return res.status(500).send();
-  res.status(201).send(data);
-})
-
-app.put('/', (req, res) => {
-
-  const upd = updateTodo(req.body.id, storage);
-  if (!upd) return res.status(500).send();
-  res.status(204).send();
-})
-
-app.delete('/', (req, res) => {
-  const del = deleteTodo(req.body.id, storage);
-  if (!del) return res.status(500).send();
-
-  res.status(204).send();
-})
-
+app.use('/tasks', tasksRouter);
 
 app.get('/healtcheck', (req, res) => {
-  res.status(200).send()
+  res.status(200).send("it works")
 })
 
 app.listen(port, () => {
---
+++ src/modules/tasks/tasks.router.ts
@@ -0,0 +1,112 @@
+import exprees, { Request, Response } from "express";
+import * as TaskService from "./tasks.service";
+import { Task, BaseTask } from "./task.interface";
+import { Tasks } from "./tasks.interface";
+
+/**
+ * Router Definition
+ */
+export const tasksRouter = exprees.Router();
+
+/**
+ * Controller Definitions
+ */
+
+// GET All task
+tasksRouter.get('/', async (req: Request, res: Response) => {
+
+  try {
+    const tasks: Tasks = await TaskService.findAll();
+
+    res.status(200).send(tasks);
+  }
+  catch (err) {
+
+    res.status(500).send(err.message);
+  }
+
+});
+
+// GET tasks/:id
+tasksRouter.get('/:id', async (req: Request, res: Response) => {
+
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const task: Task = await TaskService.find(id);
+
+    if (!task) return res.status(404).send("item not found");
+
+    res.status(200).send(task);
+  }
+  catch (err) {
+
+    res.status(500).send(err.message);
+  }
+
+});
+
+// POST tasks
+
+tasksRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const task: BaseTask = req.body;
+
+    const newTask = await TaskService.create(task);
+
+    res.status(201).json(newTask);
+
+  } catch (err) {
+
+    res.status(500).send(err.message);
+  }
+});
+
+// PUT tasks/:id
+
+tasksRouter.put("/:id", async (req: Request, res: Response) => {
+
+  const id: number = parseInt(req.params.id, 10);
+  const taskUpdate: Task = req.body;
+
+  try {
+
+    const existingTask: Task = await TaskService.find(id);
+
+    if (existingTask) {
+
+      const updatedTask = await TaskService.update(id, taskUpdate);
+      return res.status(200).json(updatedTask);
+
+    }
+
+    const newTask = await TaskService.create(taskUpdate);
+
+    res.status(201).json(newTask);
+
+  } catch (err) {
+
+    res.status(500).send(err.message);
+  }
+});
+
+// DELETE tasks/:id
+
+tasksRouter.delete('/:id', async (req: Request, res: Response) => {
+
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+
+    await TaskService.remove(id);
+
+    res.sendStatus(204);
+
+  } catch (err) {
+
+    res.status(500).send(err.message)
+
+  }
+
+
+});
